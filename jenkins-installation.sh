a850e9b1dd6525f672f271a869bfa753e2def4f3
Sanskar Ram <71223200+sanskarram981@users.noreply.github.com>
2024-08-10 20:17:20 +0530

    Update jenkins-installation.sh

--- jenkins-installation.sh
+++ jenkins-installation.sh
@@ -3,12 +3,10 @@ RESET='\033[0m'
 
 echo "${GREEN}Welcome to jenkins installation${RESET}"
 sudo apt update
-sudo apt install openjdk-8-jdk
+sudo apt install openjdk-11-jdk
 java -version
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+cd /opt
+rm jenkins.war
+sudo wget https://get.jenkins.io/war-stable/latest/jenkins.war
+sudo java -jar jenkins.war
 echo "${GREEN}Exiting from jenkins installation${RESET}"
