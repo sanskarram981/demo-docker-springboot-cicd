a277013e7b8aaf8247642a6048201f5ff3bcc9df
Sanskar Ram <71223200+sanskarram981@users.noreply.github.com>
2024-08-10 17:46:00 +0530

    Update jenkins-installation.sh

--- jenkins-installation.sh
+++ jenkins-installation.sh
@@ -6,6 +6,6 @@ docker --version
 docker stop jenkins-container
 docker rm jenkins-container
 docker pull jenkins/jenkins:lts
-docker run -d --name jenkins-container -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins:lts
+docker run -d --name jenkins-container -p 8080:8080 -p 50000:50000 -v /var/jenkins_home_v2:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins:lts
 docker logs jenkins-container
 echo "${GREEN}Exiting from jenkins installation${RESET}"
